ef2a54eb9c72d790d443854e7b64db057af47d1f
LucasTesche1 <lucas@yujilive.com.br>
2025-10-13 13:33:32 -0300

    'LWC-components-created'

---
+++ force-app/main/default/lwc/premiumClientCard/premiumClientCard.js
@@ -0,0 +1,16 @@
+import { LightningElement, api, wire, track } from 'lwc';
+import getContactData from '@salesforce/apex/PremiumClientController.getContactData';
+
+export default class PremiumClientCard extends LightningElement {
+    @api recordId;
+    @track contact;
+
+    @wire(getContactData, { contactId: '$recordId' })
+    wiredContact({ error, data }) {
+        if (data) {
+            this.contact = data;
+        } else if (error) {
+            console.error(error);
+        }
+    }
+}
